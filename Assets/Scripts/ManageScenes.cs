ddfae5adae7e98d9ea04f9924142f99e47882617
Marcin Konopacki <marcin.kon1998@gmail.com>
2022-01-24 22:26:52 +0100

    Project Upload

---
+++ Assets/Scripts/ManageScenes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ManageScenes : MonoBehaviour
+{
+    [SerializeField]
+    ToggleGroup mapSelect;
+    public void LoadScene()
+    {
+        Debug.Log(GetSelectedToggle().name);
+        if(GetSelectedToggle().name == "Day") { SceneManager.LoadScene("Park"); }
+        if(GetSelectedToggle().name == "Dark") { SceneManager.LoadScene("DarkPark"); }
+
+    }
+    public void exit()
+    {
+        Application.Quit();
+    }
+    public void mainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void setTimeScale(float setTime)
+    {
+        Time.timeScale = setTime;
+    }
+    Toggle GetSelectedToggle()
+    {
+        Toggle[] toggles = mapSelect.GetComponentsInChildren<Toggle>();
+        foreach (var t in toggles)
+            if (t.isOn) return t;  //returns selected toggle
+        return null;           // if nothing is selected return null
+    }
+}
---
+++ Assets/Scripts/MissionManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionManager : MonoBehaviour
+{
+    [SerializeField]
+    TMPro.TextMeshProUGUI timeTextMission;
+    [SerializeField]
+    TMPro.TextMeshProUGUI grabTextMission;
+
+    [SerializeField]
+    ShowTimeOnScene showTimeOnScene;
+
+    int grabCount;
+    int grabCountNow = 0;
+    float timeCount;
+    private void Start()
+    {
+        grabCount = LevelControl.instance.GetScriptable().level;
+        timeCount = LevelControl.instance.GetScriptable().level * 10;
+    }
+    private void Update()
+    {
+        timeTextMission.text = "Time : " + System.Math.Round(showTimeOnScene.timeFloat).ToString() + "/" + timeCount.ToString();
+        if(showTimeOnScene.timeFloat >= timeCount)
+        {
+            timeTextMission.color = Color.green;
+
+        }
+        else
+        {
+            timeTextMission.color = Color.red;
+
+        }
+        grabTextMission.text = "Grab : " + grabCountNow.ToString() + "/" + grabCount.ToString();
+        if (grabCountNow >= grabCount)
+        {
+            grabTextMission.color = Color.green;
+        }
+        else
+        {
+            grabTextMission.color = Color.red;
+        }
+
+    }
+
+    public void addGrab()
+    {
+        grabCountNow++;
+    }
+}
